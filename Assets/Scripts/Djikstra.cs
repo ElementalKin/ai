c7e0ad7c2ce7223eead26848c5b393497de9d337
Edward Nichols <s199102@students.aie.edu>
2020-02-18 17:46:34 -0800

    Didn't get much done today

---
+++ Assets/Scripts/Djikstra.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.Experimental.GraphView;
+struct Node
+{
+    Vector2 position;
+    float gScore;
+    float hScore;
+    float fScore;
+    Node* previous;
+    std::vector<Edge> connections;
+};
+public class Djikstra : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
