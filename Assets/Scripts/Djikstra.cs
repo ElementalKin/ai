6d98d88f96ca7ff331192e9f2bd676c3fe75f7de
Edward Nichols <s199102@students.aie.edu>
2020-02-25 17:49:50 -0800

    Worked more of dij path finding

--- Assets/Scripts/Djikstra.cs
+++ Assets/Scripts/Djikstra.cs
@@ -5,6 +5,18 @@ using UnityEditor.Experimental.GraphView;
 
 public class Djikstra : MonoBehaviour
 {
+    //public GameObject FleeFrom;
+    //Quaternion RotationTarget;
+    //public Vector3 CurrentVelocity;
+    //private Vector3 force;
+    //private Vector3 v;
+    //public float MaxVelocity;
+    //public float Speed;
+    //private float RotationSpeed;
+    //public float weight;
+    //public float dot;
+
+
     struct Node
     {
         public Vector3 position;
@@ -12,62 +24,59 @@ public class Djikstra : MonoBehaviour
         public float gScore;
         public float fScore;
         public Node[] edges;
+        public GameObject Display;
     };
     public GameObject Terrain;
     public Rigidbody AI;
     public GameObject Target;
     public GameObject NodeDispaly;
+    public GameObject NodeDispaly2;
+    public GameObject NodeDispaly3;
+    public GameObject NodeDispaly4;
+    public GameObject NodeDispaly5;
     private Node CurrentNode;
     private Node DestinationNode;
     Node[,] path = new Node[100, 100];
+    int PathSize = 100;
 
     // Start is called before the first frame update
     void Start()
     {
-        path[50, 50].position = new Vector3(Terrain.transform.position.x,0, Terrain.transform.position.z);
-        path[50, 50].gScore = 0;
-        for (int x = 0; x < 50; x++)
+        for (int x = 0; x < PathSize; x++)
         {
-            for (int i = 0; i < 50; i++)
+            for (int i = 0; i < PathSize; i++)
             {
-                path[50 + i, 50 + x].position = new Vector3(path[50, 50].position.x + i,0, path[50, 50].position.z + x);
-                path[50 + i, 50 + x].gScore = 0;
-                AddEdges(path, 50+ i, 50+ x);
-                path[50 + i, 50 + x].visited = false;
-                path[50 - i, 50 - x].position = new Vector3(path[50, 50].position.x - i,0, path[50, 50].position.z - x);
-                path[50 - i, 50 - x].gScore = 0;
-                AddEdges(path, 50 - i, 50 - x);
-                path[50 - i, 50 - x].visited = false;
-                path[50 - i, 50 + x].position = new Vector3(path[50, 50].position.x - i,0, path[50, 50].position.z + x);
-                path[50 - i, 50 + x].gScore = 0;
-                AddEdges(path, 50 - i, 50 + x);
-                path[50 - i, 50 + x].visited = false;
-                path[50 + i, 50 - x].position = new Vector3(path[50, 50].position.x + i,0, path[50, 50].position.z - x);
-                path[50 + i, 50 - x].gScore = 0;
-                AddEdges(path, 50 + i, 50 - x);
-                path[50 + i, 50 - x].visited = false;
+                path[i, x].position = new Vector3(-PathSize/2 + i,0, -PathSize / 2 + x);
+                path[i, x].gScore = 0;
+                path[i, x].visited = false;
             }
         }
         for (int x = 0; x < 100; x++)
         {
             for (int i = 0; i < 100; i++)
             {
-                Instantiate(NodeDispaly, path[i,x].position, Quaternion.identity);
+                AddEdges(path,i, x );
+                if (path[i, x].visited == false)
+                { path[i, x].Display = NodeDispaly; Instantiate(path[i, x].Display, path[i, x].position, Quaternion.identity); }
             }
         }
-        CurrentNode = path[50, 50];
         InvokeRepeating("FindPath", 0, 1.0f);
     }
     void AddEdges(Node[,] main, int x, int y)
     {
-        if ( y + 1 > 100) { }
-        else {main[x, y].edges[0] = main[x , y + 1];}
-        if (x + 1 > 100) { }
-        else { main[x, y].edges[2] = main[x + 1, y]; }
+        main[x, y].edges = new Node[4];
+        if (y + 1 >= 100) { }
+        else { main[x, y].edges[0] = main[x, y + 1];}
+
+        if (x + 1 >= 100) { }
+        else { main[x, y].edges[2] = main[x + 1, y];}
+
         if (y - 1 < 0) { }
-        else {main[x, y].edges[3] = main[x, y - 1];}
+        else { main[x, y].edges[3] = main[x, y - 1];}
+
         if (x - 1 < 0) { }
-        else {main[x, y].edges[1] = main[x - 1, y]; }
+        else { main[x, y].edges[1] = main[x - 1, y];}
+        
     }
     
 
@@ -83,28 +92,63 @@ public class Djikstra : MonoBehaviour
             for (int i = 0; i < 100; i++)
             {
                 if (Vector3.Distance(path[i,x].position, AI.transform.position) < Vector3.Distance(CurrentNode.position, AI.transform.position)) { CurrentNode = path[i, x]; }
+                if (Vector3.Distance(path[i, x].position, Target.transform.position) < Vector3.Distance(CurrentNode.position, Target.transform.position)) { DestinationNode = path[i, x]; }
             }
         }
-        CheckEdges(CurrentNode, CurrentNode.gScore);
+        CheckEdges(CurrentNode);
     }
-    void CheckEdges(Node main, float s)
+    void CheckEdges(Node main)
     {
         for (int i = 0; i < 4; i++)
         {
-            if(main.edges[i].visited == false)
+            if (main.edges[i].edges != null)
             {
-                main.edges[i].gScore = s + 1;
-                main.edges[i].visited = true;
+                if (main.edges[i].visited == false)
+                {
+                    main.edges[i].gScore = main.gScore + 1;
+                    main.edges[i].visited = true;
+                    Instantiate(NodeDispaly2, main.edges[i].position, Quaternion.identity);
+
+                    if (main.edges[i].Equals(DestinationNode))
+                    {
+                        CalculateWeight(DestinationNode);
+                        break;
+                    }
+
+
+                }
             }
-            CheckEdges(main.edges[i], s + 1);
+        }
+        for(int i = 0; i < 4; i++)
+        {
+            if (main.edges[i].visited == false)
+            {
+                if (main.edges[i].edges != null)
+                {
+                    CheckEdges(main.edges[i]);
+
+                }
+            }
         }
     }
-    void Move()
+    void CalculateWeight(Node main)
     {
-        float dot = Vector3.Dot(transform.forward,(DestinationNode.position - transform.position).normalized);
-        if (dot < .9f) { transform.Rotate(new Vector3(0, 1, 0)); }
-        //else if (dot < 0f) { transform.Rotate(new Vector3(0, -1, 0)); }
-        //if(transform.position != DestinationNode.position) { AI.AddForce(transform.forward, ForceMode.VelocityChange); }
-
+        for (int i = 0; i < 4; i++)
+        {
+            //if (main.edges[i].gScore)
+        }
     }
+    void Move(Node targetNode)
+    {
+        //float dot = Vector3.Dot(transform.forward,(DestinationNode.position - transform.position).normalized);
+        //if (dot < .9f) { transform.Rotate(new Vector3(0, 1, 0)); }
+        //else if (dot < 0f) { transform.Rotate(new Vector3(0, -1, 0)); }
+        //if(transform.position != targetNode.position) { AI.AddForce(transform.forward, ForceMode.VelocityChange); }
+        //v = ((targetNode.position - transform.position) * MaxVelocity).normalized;
+        //force = v - CurrentVelocity;
+        //CurrentVelocity += force * Time.deltaTime;
+        //transform.position += CurrentVelocity * Time.deltaTime;
+        //Quaternion.RotateTowards(transform.rotation,new Quaternion(CurrentVelocity.x,CurrentVelocity.y,CurrentVelocity.z,0),10);
+    }
+
 }
