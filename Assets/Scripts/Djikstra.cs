caa8d5a9e7dd8213e836bafd584c137fcf406ec4
Edward Nichols <s199102@students.aie.edu>
2020-03-02 17:27:38 -0800

    finishing up with dij just need to get one bug done the n working on wieghing the node.

--- Assets/Scripts/Djikstra.cs
+++ Assets/Scripts/Djikstra.cs
@@ -1,7 +1,67 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor.Experimental.GraphView;
+
+
+//public class Node<Location>
+//{
+//    NameValueCollection would be a reasonable alternative here, if
+//     you're always using string location types
+//    public Dictionary<Location, Location[]> edges
+//        = new Dictionary<Location, Location[]>();
+
+//    public Location[] Neighbors(Location id)
+//    {
+//        return edges[id];
+//    }
+//};
+
+//public class Djikstra : MonoBehaviour
+//{
+//    public GameObject Terrain;
+//    public Rigidbody AI;
+//    public GameObject Target;
+//    public GameObject NodeDispaly;
+//    public GameObject NodeDispaly2;
+//    static void Search(Node<string> graph, string start)
+//    {
+//        var frontier = new Queue<string>();
+//        frontier.Enqueue(start);
+
+//        var visited = new HashSet<string>();
+//        visited.Add(start);
+
+//        while (frontier.Count > 0)
+//        {
+//            var current = frontier.Dequeue();
+
+//            Debug.Log("Visiting {0}" + current);
+//            foreach (var next in graph.Neighbors(current))
+//            {
+//                if (!visited.Contains(next))
+//                {
+//                    frontier.Enqueue(next);
+//                    visited.Add(next);
+//                }
+//            }
+//        }
+//    }
+
+//    void Start()
+//    {
+//        Node<string> g = new Node<string>();
+//        g.edges = new Dictionary<string, string[]>
+//            {
+//            { "A", new [] { "B" } },
+//            { "B", new [] { "A", "C", "D" } },
+//            { "C", new [] { "A" } },
+//            { "D", new [] { "E", "A" } },
+//            { "E", new [] { "B" } }
+//        };
+
+//        Search(g, "A");
+//    }
+//}
 
 
 public class Djikstra : MonoBehaviour
@@ -17,7 +77,6 @@ public class Djikstra : MonoBehaviour
     //public float weight;
     //public float dot;
 
-
     struct Edges
     {
         public int x, y;
@@ -26,6 +85,7 @@ public class Djikstra : MonoBehaviour
     {
         public Vector3 position;
         public bool visited;
+        public bool IsAssigned;
         public float gScore;
         public float fScore;
         public Edges[] edges;
@@ -51,9 +111,10 @@ public class Djikstra : MonoBehaviour
         {
             for (int i = 0; i < PathSize; i++)
             {
-                path[i, x].position = new Vector3(-PathSize/2 + i,0, -PathSize / 2 + x);
+                path[i, x].position = new Vector3(-PathSize / 2 + i, 0, -PathSize / 2 + x);
                 path[i, x].gScore = 0;
                 path[i, x].visited = false;
+                path[i, x].IsAssigned = true;
             }
         }
         for (int x = 0; x < PathSize; x++)
@@ -61,37 +122,50 @@ public class Djikstra : MonoBehaviour
             for (int i = 0; i < PathSize; i++)
             {
                 AddEdges(path, i, x);
-                //path[i, x].Display = NodeDispaly; Instantiate(path[i, x].Display, path[i, x].position, Quaternion.identity); 
+                path[i, x].Display = NodeDispaly; Instantiate(path[i, x].Display, path[i, x].position, Quaternion.identity); 
             }
         }
         FindPath();
     }
     void AddEdges(Node[,] main, int x, int y)
     {
-        main[x, y].edges = new Edges[4]; 
+        main[x, y].edges = new Edges[4];
         if (y + 1 >= PathSize) { }
-        else { main[x, y].edges[0].x = x;
-               main[x, y].edges[0].x = y + 1;}
+        else
+        {
+            main[x, y].edges[0].x = x;
+            main[x, y].edges[0].y = y + 1;
+        }
 
         if (x + 1 >= PathSize) { }
-        else { main[x, y].edges[2].x = x + 1;
-               main[x, y].edges[2].y = y;}
+        else
+        {
+            main[x, y].edges[2].x = x + 1;
+            main[x, y].edges[2].y = y;
+        }
 
         if (y - 1 < 0) { }
-        else { main[x, y].edges[3].x = x;
-               main[x, y].edges[3].y = y- 1;}
+        else
+        {
+            main[x, y].edges[3].x = x;
+            main[x, y].edges[3].y = y - 1;
+        }
 
         if (x - 1 < 0) { }
-        else { main[x, y].edges[1].x = x -1;
-               main[x, y].edges[1].y = y;}
+        else
+        {
+            main[x, y].edges[1].x = x - 1;
+            main[x, y].edges[1].y = y;
+        }
 
     }
-    
+
 
 
     // Update is called once per frame
     void Update()
     {
+
     }
     void FindPath()
     {
@@ -99,71 +173,55 @@ public class Djikstra : MonoBehaviour
         {
             for (int i = 0; i < 100; i++)
             {
-                if (Vector3.Distance(path[i,x].position, AI.transform.position) < Vector3.Distance(CurrentNode.position, AI.transform.position)) { CurrentNode = path[i, x]; }
+                if (Vector3.Distance(path[i, x].position, AI.transform.position) < Vector3.Distance(CurrentNode.position, AI.transform.position)) { CurrentNode = path[i, x]; }
                 if (Vector3.Distance(path[i, x].position, Target.transform.position) < Vector3.Distance(CurrentNode.position, Target.transform.position)) { DestinationNode = path[i, x]; }
             }
         }
-        CheckEdges(CurrentNode, path);
+        Queue<Node> ToDoList = new Queue<Node>();
+        Queue<Vector3> Done = new Queue<Vector3>();
+        ToDoList.Enqueue(CurrentNode);
+        CheckEdges(CurrentNode, path, ToDoList, Done, 0);
     }
-    void CheckEdges(Node main, Node[,] path)
+    void CheckEdges(Node main, Node[,] path, Queue<Node> ToDoList, Queue<Vector3> Done, int a)
     {
-        if (main.Equals(CurrentNode))
+        if (main.IsAssigned)
         {
-            for (int i = 0; i < 4; i++)
+            if (!Done.Contains(main.position))
             {
-                if (path[main.edges[i].x, main.edges[i].y].edges != null)
+                if (main.Equals(CurrentNode))
                 {
-                    if (path[main.edges[i].x, main.edges[i].y].visited == false)
+                    main.gScore = 0;
+                    Instantiate(NodeDispaly2, main.position, Quaternion.identity);
+
+                    if (main.Equals(DestinationNode))
                     {
+                        CalculateWeight(DestinationNode);
+                    }
+                    Done.Enqueue(main.position);
+                }
+                else
+                {
 
-                        path[main.edges[i].x, main.edges[i].y].gScore = main.gScore + 1;
-                        Instantiate(NodeDispaly2, path[main.edges[i].x, main.edges[i].y].position, Quaternion.identity);
+                    main.gScore = a + 1;
+                    //Instantiate(NodeDispaly2, main.position, Quaternion.identity);
 
-                        if (main.edges[i].Equals(DestinationNode))
-                        {
-                            CalculateWeight(DestinationNode);
-                            break;
-                        }
-
-
+                    if (main.Equals(DestinationNode))
+                    {
+                        CalculateWeight(DestinationNode);
+                    }
+                    Done.Enqueue(main.position);
+                }
+                for (int i = 0; i < 4; i++)
+                {
+                    if (!Done.Contains(path[main.edges[i].x, main.edges[i].y].position))
+                    {
+                        ToDoList.Enqueue(path[main.edges[i].x, main.edges[i].y]);
                     }
                 }
+                
             }
-            main.visited = true;
-        }
-        else
-        {
-            for (int i = 0; i < 4; i++)
-            {
-                if (path[main.edges[i].x, main.edges[i].y].edges != null)
-                {
-                    if (path[main.edges[i].x, main.edges[i].y].visited == false)
-                    {
-                        path[main.edges[i].x, main.edges[i].y].gScore = main.gScore + 1;
-                        Instantiate(NodeDispaly2, path[main.edges[i].x, main.edges[i].y].position, Quaternion.identity);
-
-                        if (main.edges[i].Equals(DestinationNode))
-                        {
-                            CalculateWeight(DestinationNode);
-                            break;
-                        }
-
-
-                    }
-                }
-            }
-        }
-        for(int i = 0; i < 4; i++)
-        {
-            if (path[main.edges[i].x, main.edges[i].y].visited == false)
-            {
-                if (path[main.edges[i].x, main.edges[i].y].edges != null)
-                {
-                    path[main.edges[i].x, main.edges[i].y].visited = true;
-                    CheckEdges(path[main.edges[i].x, main.edges[i].y], path);
-
-                }
-            }
+            ToDoList.Dequeue();
+            CheckEdges(ToDoList.Peek(), path, ToDoList, Done, a);
         }
     }
     void CalculateWeight(Node main)
